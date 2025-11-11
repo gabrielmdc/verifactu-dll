f233722e4508d0683afe0473f15cada5f8c9a16d
Gabriel Mateos <gmdcwork@gmail.com>
2025-11-12 00:03:43 +0100

    fix: fix Verifactu class

--- src/Verifactu.cs
+++ src/Verifactu.cs
@@ -6,32 +6,36 @@ namespace VerifactuDll;
 public class Verifactu
 {
 	// Returns lowercase hex SHA-256 of the UTF-8 bytes of msg
-	private static string GetMessageHash(string msg)
+	public static string GetMessageHash(string msg)
 	{
-		using var sha256 = SHA256.Create();
-		var hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(msg));
-		var sb = new StringBuilder(hash.Length * 2);
+		var sb = new StringBuilder();
 
-		foreach (var b in hash)
+		using (var sha256 = SHA256.Create())
 		{
-			sb.Append(b.ToString("x2")); // lowercase hex
+			// Java: new Base16(false).encodeAsString(...) => uppercase hex
+			var hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(msg));
+
+			foreach (var b in hash)
+			{
+				sb.Append(b.ToString("X2")); // uppercase hex
+			}
 		}
 
 		return sb.ToString();
 	}
 
-	private static String GetReferenciaRegistroAlta(String nifEmisor, String numFacturaSerie, String fechaExpedicion, String tipoFactura, String cuotaTotal, String importeTotal, String huellaAnterior, String fechaHoraUsoRegistro)
+	public static String GetReferenciaRegistroAlta(String nifEmisor, String numFacturaSerie, String fechaExpedicion, String tipoFactura, String cuotaTotal, String importeTotal, String huellaAnterior, String fechaHoraUsoRegistro)
 	{
 		var fieldList = new[]
 		{
-			GetFieldValue("NIFEmisor", nifEmisor),
+			GetFieldValue("IDEmisorFactura", nifEmisor),
 			GetFieldValue("NumSerieFactura", numFacturaSerie),
 			GetFieldValue("FechaExpedicionFactura", fechaExpedicion),
 			GetFieldValue("TipoFactura", tipoFactura),
 			GetFieldValue("CuotaTotal", cuotaTotal),
 			GetFieldValue("ImporteTotal", importeTotal),
 			GetFieldValue("Huella", huellaAnterior),
-			GetFieldValue("FechaHoraUsoGenRegistro", fechaHoraUsoRegistro)
+			GetFieldValue("FechaHoraHusoGenRegistro", fechaHoraUsoRegistro)
 		};
 
 		return String.Join("&", fieldList);
@@ -49,7 +53,7 @@ public class Verifactu
 	{
 		String str = GetReferenciaRegistroAlta(nifEmisor, numFacturaSerie,
 			fechaExpedicion.ToString("dd-MM-yyyy"), tipoFactura, cuotaTotal, importeTotal,
-			huellaAnterior, fechaHoraUsoRegistro.ToString("yyyy-MM-ddTHH:mm:ss"));
+			huellaAnterior, fechaHoraUsoRegistro.ToString("yyyy-MM-ddTHH:mm:ssK"));
 
 		return GetMessageHash(str);
 	}
